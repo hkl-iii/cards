5e5ddc70722a49767b79133f1caa484c58a1b4b2
hkl-iii <rahleggon@gmail.com>
2021-05-01 23:01:43 -0500

    made new function inside of deckgo file line 9

--- deck.go
+++ deck.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+//create a new type of 'deck´
+//which is a slice of strings
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
