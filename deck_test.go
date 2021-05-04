3f10c1179766e9b723b79a5633f242ab9a1edea5
hkl-iii <rahleggon@gmail.com>
2021-05-03 22:30:53 -0500

    added %v line 11 testgo file so that the gotest would successfully run. Test passed

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
